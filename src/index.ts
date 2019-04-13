0d6d38839648c37626e5724d1ee64c12b9bb0cf8
Darren Karl Sapalo <darren.sapalo@gmail.com>
2019-04-13 17:03:31 +0800

    Updated code for main file.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,23 @@
 import express from 'express';
 import { fetchRecentMatches } from './dota';
-import './calendar';
-
+import { insertNewDotaMatchesAsCalendarEvents } from './calendar';
 /**
  * Responds to any HTTP request.
  *
  * @param {!express:Request} req HTTP request context.
  * @param {!express:Response} res HTTP response context.
  */
-exports.parseDotaGames = (request: express.Request, response: express.Response) => {
-    let limit = request.query.limit || request.body.limit || 5;
+exports.parseDotaGames = (_request: express.Request, response: express.Response) => {
+    
+    fetchRecentMatches()
+        .pipe(
+            insertNewDotaMatchesAsCalendarEvents()
+        )
+        .subscribe(events => {
+            response.status(200).send(`Created a total of ${events.length} event/s.`);
+        }, err => {
+            let errorMesasge = err.message || err;
+            response.status(500).send(`An error ocurred: ${errorMesasge}`);
+        });
 
-    fetchRecentMatches().subscribe(console.log, console.error);
-
-    response.status(200).send(limit);
 };
-
-// fetchRecentMatches().subscribe(console.log, console.error);
