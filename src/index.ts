9bcde0d7ba3c23549a59cc448f653b99feee53d6
Darren Karl A. Sapalo <darren.sapalo@gmail.com>
2019-04-17 05:10:25 +0800

    Cleaned up index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,22 +14,14 @@ exports.parseDotaGames = (_request: express.Request, response: express.Response)
             insertNewDotaMatchesAsCalendarEvents()
         )
         .subscribe(events => {
-            response.status(200).send(`Created a total of ${events.length} event/s.`);
+            response.status(200).json({
+                message: 'success',
+                total: events.length,
+                events: events
+            });
         }, err => {
             let errorMesasge = err.message || err;
             response.status(500).send(`An error ocurred: ${errorMesasge}`);
         });
 
 };
-
-
-fetchRecentMatches()
-    .pipe(
-        insertNewDotaMatchesAsCalendarEvents()
-    )
-    .subscribe(events => {
-        console.log(`Created a total of ${events.length} event/s.`);
-    }, err => {
-        let errorMesasge = err.message || err;
-        console.log(`An error ocurred: ${errorMesasge}`);
-    });
